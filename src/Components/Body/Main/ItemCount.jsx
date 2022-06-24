bc8cd93f95545316db1a3e563261fb1e8d437ba4
nmuradas <nestormuradas@gmail.com>
2022-06-24 18:27:27 -0300

    Subo correcciones a la entrega del contador. para que valide que el carrito este vacio

--- src/Components/Body/Main/ItemCount.jsx
+++ src/Components/Body/Main/ItemCount.jsx
@@ -11,6 +11,11 @@ const ItemCount = ({stock, initial, onAdd})=> {
     
     const quitar = () => {
         setContador(contador>0 ? contador - 1 : 0)}
+
+    const vaciar = () => {
+        localStorage.setItem("cantidad", 0)
+        document.location.reload();
+        alert("Su carrito de compras ahora se encuentra vacio")}
         
     
     return (
@@ -20,6 +25,7 @@ const ItemCount = ({stock, initial, onAdd})=> {
             <button onClick={agregar} >Agregar productos</button>
             <button onClick={quitar} >Quitar productos</button>
             <button onClick={()=>{contador>0 ? onAdd(contador) : alert("No hay productos en el carrito")}}>Agregar al carrito</button>
+            <button onClick={()=>{localStorage.cantidad>0 ? vaciar() : alert("No hay productos en el carrito")}}>Vaciar Carrito</button>
 
         </>
     )
