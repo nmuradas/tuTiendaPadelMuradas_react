e57ad6080ee8757bdc2f27e4e58305a5e6e6e36a
nmuradas <nestormuradas@gmail.com>
2022-06-24 18:50:30 -0300

    subo ultima correcion de la entrega de contador

--- src/Components/Body/Main/ItemCount.jsx
+++ src/Components/Body/Main/ItemCount.jsx
@@ -19,17 +19,32 @@ const ItemCount = ({stock, initial, onAdd})=> {
         
     
     return (
-        <>
-            <div>Mis productos</div>
+        <section style={styles.container}>
+            <div>MIS PRODUCTOS</div>
             <div>{contador}</div>
-            <button onClick={agregar} >Agregar productos</button>
-            <button onClick={quitar} >Quitar productos</button>
+            <br />
+            <button onClick={agregar} >➕</button>
+            <button onClick={quitar} >➖</button> <br /> <br />
             <button onClick={()=>{contador>0 ? onAdd(contador) : alert("No hay productos en el carrito")}}>Agregar al carrito</button>
             <button onClick={()=>{localStorage.cantidad>0 ? vaciar() : alert("No hay productos en el carrito")}}>Vaciar Carrito</button>
-
-        </>
+        </section>
     )
 
 }
 
 export default ItemCount
+
+const styles = {
+    container: {
+        backgroundColor:"gray",
+        maxWidth:340,
+        alignItems: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 10,
+        borderRadius: 15,
+        borderColor: 'blue', 
+        borderWidth: 5,
+        borderStyle: 'solid', 
+        fontWeight: 'bold', 
+    },}
